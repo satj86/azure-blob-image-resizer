3196290d2270ffcc0036ebda895f89607014f137
satj86 <ssjootle@gmail.com>
2019-02-18 20:56:35 +0000

    setting

--- AzureBlobImageResizer/ImageResizer.cs
+++ AzureBlobImageResizer/ImageResizer.cs
@@ -8,7 +8,7 @@ namespace AzureBlobImageResizer
     public static class ImageResizer
     {
         [FunctionName("ImageResizer")]
-        public static void Run([BlobTrigger("unprocessed/{name}", Connection = "DefaultEndpointsProtocol=https;AccountName=receiptsstorage;AccountKey=3WAk2ptGfORv1XK1G1OUHOcFQPl7EOXdUcdxzprFOz5Qd1Wh3IuhwkrPtb9YjU0iDd1T8aegH0ylWFn+ai3Gzw==;EndpointSuffix=core.windows.net")]Stream myBlob, string name, ILogger log)
+        public static void Run([BlobTrigger("unprocessed/{name}", Connection = "connie")]Stream myBlob, string name, ILogger log)
         {
             log.LogInformation($"C# Blob trigger function Processed blob\n Name:{name} \n Size: {myBlob.Length} Bytes");
         }
